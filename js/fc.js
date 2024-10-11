34949a4213ed4cd02497c24d230d1f34abae2a07
Masayuki Yamana <myamana@gmail.com>
2024-10-11 16:03:28 +0900

    added clear button

--- js/fc.js
+++ js/fc.js
@@ -1,23 +1,51 @@
 function updateTotal() {
-    console.log('call updateTotal');
 
-    console.log($('#ff-num').val());
-    let ffTotal = 200 * $('#ff-num').val();
+
+    let ffNum = 0;
+    if ($('#ff-num').val() != '') {
+        ffNum = $('#ff-num').val();
+    }
+    let ffTotal = 200 * ffNum;
     $('#ff-total').text(ffTotal);
 
-    let hdTotal = 250 * $('#hd-num').val();
+    let hdNum = 0;
+    if ($('#hd-num').val() != '') {
+        hdNum = $('#hd-num').val();
+    }
+    let hdTotal = 250 * hdNum;
     $('#hd-total').text(hdTotal);
 
-    let okTotal = 250 * $('#ok-num').val();
+    let okNum = 0;
+    if ($('#ok-num').val() != '') {
+        okNum = $('#ok-num').val();
+    }
+    let okTotal = 250 * okNum;
     $('#ok-total').text(okTotal);
 
-    let krTotal = 100 * $('#kr-num').val();
+    let krNum = 0;
+    if ($('#kr-num').val() != '') {
+        krNum = $('#kr-num').val();
+    }
+    let krTotal = 100 * krNum;
     $('#kr-total').text(krTotal);
 
-    let suiTotal = 100 * $('#sui-num').val();
+    let suiNum = 0;
+    if ($('#sui-num').val() != '') {
+        suiNum = $('#sui-num').val();
+    }
+    let suiTotal = 100 * suiNum;
     $('#sui-total').text(suiTotal);
 
     let total = ffTotal + hdTotal + okTotal + krTotal + suiTotal;
     $('#total').val(total);
 
+}
+
+function clearTotal() {
+    $('#ff-num').val('');
+    $('#hd-num').val('');
+    $('#ok-num').val('');
+    $('#kr-num').val('');
+    $('#sui-num').val('');
+    updateTotal();
 }
